169b2a3da7e4d1b513ad86e37a9629f95243ec71
OpenMirror <54529482+OpenMirror@users.noreply.github.com>
2023-06-04 17:08:52 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
     // The global $_POST variable allows you to access the data sent with the POST method by name
     // To access the data sent with the GET method, you can use $_GET
-    $login = htmlspecialchars($_POST['login']);
+    $login = $_POST['login'];
     $password  = htmlspecialchars($_POST['password']);
 
     $row = 1;
