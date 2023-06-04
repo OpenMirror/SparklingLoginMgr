6de86287f66f86bf88fdd4d2c13a5b169110d1bc
OpenMirror <54529482+OpenMirror@users.noreply.github.com>
2023-06-04 17:11:40 +0200

    Update login.php

--- login.php
+++ login.php
@@ -17,6 +17,7 @@
 
     echo '<pre>'; print_r($mycsvfile); echo '</pre>';
     $i = 0;
+    echo $mycsvfile[$i][1]
     while ($mycsvfile[$i][0] !== $login) {
         
         if ($mycsvfile[$i][0] == $login) {
