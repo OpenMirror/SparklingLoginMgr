b2d1b4dd0a41649dc511b57c4630396ec54abeb9
OpenMirror <54529482+OpenMirror@users.noreply.github.com>
2023-06-05 10:13:49 +0200

    Update login.php

--- login.php
+++ login.php
@@ -36,4 +36,12 @@
     
 //     exit;
 ?>
+ <!DOCTYPE html>
+<html>
+<body style="margin: 0; padding: 0;">
+
 <iframe width="100%" height="100%" frameborder="0" src="<?php echo $ip_to_send; ?>"></iframe>
+
+</body>
+</html> 
+
